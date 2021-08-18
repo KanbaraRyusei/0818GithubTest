3966a1d5a69b06151b247976cf5ad4ad823881e1
Ryusei1115 <tekunikaru22@gmail.com>
2021-08-18 13:19:41 +0900

    スクリプトを変更した

--- Assets/Scenes/Test.cs
+++ Assets/Scenes/Test.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
+    int a = 0;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("test");
+        a = 5;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(a);   
     }
 }
